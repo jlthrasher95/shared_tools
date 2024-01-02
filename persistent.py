09572f61d84393a4fa08316b7ea9fa783bcbbdce
BuildTools <unconfigured@null.spigotmc.org>
2024-01-02 16:09:49 -0500

    added return to persistent.Session.quit_check()

--- persistent.py
+++ persistent.py
@@ -40,6 +40,9 @@ class Session():
 
     def quit_check(self, string):
         """This method checks a string for quit conditions."""
+        has_quit = False
         quit_conditions = ('q', 'quit')
         if string in quit_conditions:
+            has_quit = True
             self.end()
+        return has_quit
